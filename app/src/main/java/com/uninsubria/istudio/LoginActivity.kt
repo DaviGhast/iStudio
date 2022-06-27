1841774fc0919c795a356a70d1eb1b9f216756bd
lucamuggiasca <lucamuggiasca@outlook.com>
2022-06-27 20:21:36 +0200

    LoginActivity.kt and login_activity.xml modified

--- app/src/main/java/com/uninsubria/istudio/LoginActivity.kt
+++ app/src/main/java/com/uninsubria/istudio/LoginActivity.kt
@@ -16,29 +16,35 @@ class LoginActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
         auth = FirebaseAuth.getInstance()
-    }
 
-    fun login(view: View) {
-        val email = etemail.text.toString()
-        val password = etpassword.text.toString()
-        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                Log.d(TAG, "signInWithEmail:success")
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-                finish()
-            }else{
-                Log.d(TAG, "signInWithEmail:failure",task.exception)
-                Toast.makeText(this, "Authentication failed.",
-                    Toast.LENGTH_SHORT).show()
+
+        fun login(view: View) {
+            val email = etemail.text.toString()
+            val password = etpassword.text.toString()
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "signInWithEmail:success")
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    Log.d(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        this, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
             }
         }
-    }
-    fun goToRegister(view:View){
-        tvRegister.setOnClickListener(){
-            val intent=Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
+
+        loginbutton.setOnClickListener(){
+            login(loginbutton)
         }
 
+        tvRegister.setOnClickListener(){
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
