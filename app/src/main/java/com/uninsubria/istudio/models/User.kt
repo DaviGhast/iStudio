ad250d21e0b12b88c2aadd7e1ab1d83e89ef6160
DaviGhast <davide.mainardi2001@hotmail.com>
2022-07-02 11:22:30 +0200

    Recreated RegisterActivity.kt: update google-services.json, added dependencies in gradle, added animations and activity xml pages, redesigned themes and created user model

---
+++ app/src/main/java/com/uninsubria/istudio/models/User.kt
@@ -0,0 +1,13 @@
+package com.uninsubria.istudio.models
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class User(
+    val uid: String,
+    val name: String,
+    val profileImageUrl: String?
+) : Parcelable {
+    constructor() : this("", "", "")
+}
