388a44bdcd8153a21719302ce10d3be3459be972
brennofi <orgiore69@gmail.com>
2022-08-05 17:25:10 +0200

    activity_post.xml layout modified. added intent to MapsActivity in PostActivity.kt.

--- app/src/main/java/com/uninsubria/istudio/ui/PostActivity.kt
+++ app/src/main/java/com/uninsubria/istudio/ui/PostActivity.kt
@@ -1,13 +1,20 @@
 package com.uninsubria.istudio.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.uninsubria.istudio.R
+import com.uninsubria.istudio.ui.login.LoginActivity
+import kotlinx.android.synthetic.main.activity_post.*
 
 class PostActivity: AppCompatActivity() {
 
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_post)
+            maplink.setOnClickListener(){
+                val intent = Intent(this, MapsActivity::class.java)
+                startActivity(intent)
+            }
         }
 }
