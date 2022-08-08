568a853a0f8c4c1d72326d045576a39006165e48
lucamuggiasca <lucamuggiasca@outlook.com>
2022-08-08 17:21:34 +0200

    added GPS current location

--- app/src/main/java/com/uninsubria/istudio/ui/NewPostActivity.kt
+++ app/src/main/java/com/uninsubria/istudio/ui/NewPostActivity.kt
@@ -1,10 +1,19 @@
 package com.uninsubria.istudio.ui
 
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.firebase.database.FirebaseDatabase
 import com.uninsubria.istudio.R
 import com.uninsubria.istudio.messages.NewMessageActivity
@@ -16,6 +25,9 @@ import kotlinx.android.synthetic.main.activity_register.*
 import kotlinx.android.synthetic.main.post_row.*
 
 class NewPostActivity: AppCompatActivity() {
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    lateinit var longitude:String
+    lateinit var latitude:String
 
     private val currentUser: User?
         get() = intent.getParcelableExtra("USER_KEY")
@@ -25,9 +37,90 @@ class NewPostActivity: AppCompatActivity() {
         ettext.text.clear()
         post_checkBox.isChecked = false
     }
+    private fun getCurrentLocation(){
+        if(checkPermissions())
+        {
+            if (isLocationEnabled())
+            {
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener(this){task->
+                    val location: Location?=task.result
+                    if (location==null){
+                        Toast.makeText(this, "Null Received",Toast.LENGTH_SHORT).show()
+                    }
+                    else{
+                        Toast.makeText(this, "Get Success",Toast.LENGTH_SHORT).show()
+                        latitude=""+location.latitude
+                        longitude=""+location.longitude
+                    }
+                }
+
+            }
+            else
+            {
+                Toast.makeText(this,"Abilita la localizzazione",Toast.LENGTH_SHORT).show()
+                val intent=Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        }
+        else
+        {
+            requestPermission()
+        }
+
+
+    }
+    private fun  isLocationEnabled():Boolean{
+        val locationManager:LocationManager=getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(this,
+            arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION,android.Manifest.permission.ACCESS_FINE_LOCATION),
+        PERMISSION_REQUEST_ACCESS_LOCATION)
+    }
+
+    companion object{
+        private const val PERMISSION_REQUEST_ACCESS_LOCATION=100
+    }
+    private fun checkPermissions():Boolean{
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+            ==PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission
+            (this, android.Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED)
+        {
+            return true
+        }
+        return false
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode== PERMISSION_REQUEST_ACCESS_LOCATION){
+            if (grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(applicationContext,"Accesso garantito",Toast.LENGTH_SHORT).show()
+                getCurrentLocation()
+            }
+            else{
+                Toast.makeText(applicationContext,"Accesso negato",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_post)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        getCurrentLocation()
 
 
         buttonDelete.setOnClickListener(){
