c9ac596da31daa9ded7ea965bee1d83ce20ed58f
lucamuggiasca <lucamuggiasca@outlook.com>
2022-08-29 17:51:27 +0200

    fixed PostActivity.kt

--- app/src/main/java/com/uninsubria/istudio/posts/PostActivity.kt
+++ app/src/main/java/com/uninsubria/istudio/posts/PostActivity.kt
@@ -1,15 +1,21 @@
 package com.uninsubria.istudio.posts
 
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.uninsubria.istudio.R
 import com.uninsubria.istudio.models.Post
+import com.uninsubria.istudio.models.User
 import com.uninsubria.istudio.ui.HomeActivity
 import com.uninsubria.istudio.ui.fragments.SecondFragment
 import kotlinx.android.synthetic.main.activity_post.*
+import kotlinx.android.synthetic.main.fragment_profile.view.*
+import java.net.URI
 
 
 class PostActivity: AppCompatActivity() {
@@ -23,12 +29,30 @@ class PostActivity: AppCompatActivity() {
 
         val array = intent.getParcelableArrayExtra("USER_KEY")
         val mypost: Post = array?.get(1) as Post
+        val myuser: User = array?.get(0) as User
+
         titleap.text = mypost.title
         textap.text = mypost.text
+        postusername.text = myuser.name
+        maplink.text= "See ${myuser.name} position"
+
+        if (myuser.profileImageUrl?.isEmpty()!!) {
+            val requestOptions = RequestOptions().placeholder(R.drawable.no_image2)
+            Glide.with(this)
+                .load(myuser.profileImageUrl)
+                .apply(requestOptions)
+                .into(userprofileimage)
+        }
+        else {
+            Glide.with(this)
+            .load(myuser.profileImageUrl)
+            .into(userprofileimage)
+        }
+
 
         val latitude = mypost.latitude
         val longitude = mypost.longitude
-        val label = ""
+
 
         if (latitude==""||longitude==""){
             maplink.isVisible = !maplink.isVisible
@@ -41,10 +65,6 @@ class PostActivity: AppCompatActivity() {
             val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
             mapIntent.setPackage("com.google.android.apps.maps")
             mapIntent.resolveActivity(packageManager)?.let { startActivity(mapIntent) }
-        }
-        returnbutton.setOnClickListener(){
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
         }
     }
 
